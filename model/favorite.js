9ed15dfcd0063403825753c7b0b76cb7568fd81a
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-09 11:18:06 +0545

    Update favorite model to enhance schema and relations

--- model/favorite.js
+++ model/favorite.js
@@ -3,14 +3,18 @@ const mongoose = require("mongoose");
 const favoriteSchema = new mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Customer", // Reference to the user
+    ref: "Customer",
     required: true,
   },
   book_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Book", // Reference to the book
+    ref: "Book",
     required: true,
   },
-});
+  isFavorite: {
+    type: Boolean,
+    default: true,
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Favorite", favoriteSchema);
