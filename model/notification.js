f2c31e7847ded186afde524bd3c5f823bc1813cd
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-15 10:32:59 +0545

    Create notification model to store user notifications

---
+++ model/notification.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "customers",
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: ["info", "success", "warning", "error"],
+      default: "info",
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    relatedId: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to the book request
+      ref: "BookRequest",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = Notification;
