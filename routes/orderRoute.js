15eb67412803308e53df2e4e7f38e7e915692c25
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-20 13:59:35 +0545

    Update orderRoute.js to refine order-related API routing

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -6,6 +6,8 @@ const {
   getAllOrders,
   updateOrderStatus,
   deleteOrder,
+  getOrderTypeCounts,
+  getCurrentlyReading // Include new function
 } = require("../controller/orderController");
 
 const { authenticateToken, authorizeRole } = require("../security/authorization");
@@ -15,5 +17,7 @@ router.get("/:user_id", authenticateToken, getOrdersByUser);
 router.get("/", authenticateToken, authorizeRole("Admin"), getAllOrders);
 router.put("/:id", authenticateToken, authorizeRole("Admin"), updateOrderStatus);
 router.delete("/:id", authenticateToken, deleteOrder);
+router.get("/counts/:user_id", authenticateToken, getOrderTypeCounts);
+router.get("/currently-reading/:user_id", authenticateToken, getCurrentlyReading); // New route
 
 module.exports = router;
